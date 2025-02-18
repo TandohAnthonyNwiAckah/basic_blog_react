075ed6da605b9731d3ba68c8c8b23b983aad62d7
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2025-02-18 14:45:39 +0000

    Pass function as props

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,37 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+  // STATE
   const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "tanacom",
+      id: 1,
+    },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "tony", id: 2 },
     {
       title: "Web dev top tips",
       body: "lorem ipsum...",
-      author: "mario",
+      author: "tanacom",
       id: 3,
     },
   ]);
 
+  // Hide blog using filter id
+  const hideBlog = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={"All Blogs"} />
+      <BlogList blogs={blogs} title={"All Blogs!"} hideBlog={hideBlog} />
+
+      {/* <BlogList
+        blogs={blogs.filter((blog) => blog.author === "tony")}
+        title="Tony's Blogs"
+      /> */}
     </div>
   );
 };
