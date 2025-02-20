275252fbd85d31d30b57c85e8a5860e19aa4eddf
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2025-02-20 16:28:56 +0000

    add delete to blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,25 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
+
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+  // Delete blog
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +30,9 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onC to lick={handleClick}>
+            delete
+          </button>
         </article>
       )}
     </div>
